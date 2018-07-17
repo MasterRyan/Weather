b9543e8297201ce37f150ee52d609c47dc94a3cd
user141656 <user141656@eu249.local>
2018-07-17 04:30:33 +0100

    adding first draft of dash ui

---
+++ CloudWeather/CloudWeather/controller/DashboardViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DashboardViewController.swift
+//  CloudWeather
+//
+//  Created by matthew.ryan on 7/17/18.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: DashboardDataController {
+
+    @IBOutlet weak var fiveDayCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //fiveDayCollectionView
+    }
+    
+    override func refresh() {
+        fiveDayCollectionView.reloadData()
+    }
+}
+
+extension DashboardViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+extension DashboardViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = collectionView.bounds.size
+        
+        return CGSize(width: (size.width / 6)  , height: size.height)
+    }
+    
+}
+
+extension DashboardViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fiveDayData?.list.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HOUR_CELL", for: indexPath)
+        
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+}
