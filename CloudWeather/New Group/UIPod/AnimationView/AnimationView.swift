8ea89636dcabe61123491bf048545af2fbe4cf90
user141656 <user141656@eu249.local>
2018-07-30 22:24:02 +0100

    set up animation view file and folder

---
+++ CloudWeather/New Group/UIPod/AnimationView/AnimationView.swift
@@ -0,0 +1,15 @@
+//
+//  AnimationView.swift
+//  CloudWeather
+//
+//  Created by user141656 on 7/30/18.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class AnimationView: UIView {
+
+
+
+}
