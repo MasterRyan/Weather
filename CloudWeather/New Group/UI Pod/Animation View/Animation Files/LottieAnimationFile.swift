5a9ac811e786961fddf371aebc35b66ea6833fa8
user141656 <user141656@eu249.local>
2018-07-31 06:41:20 +0100

    adding lottie support

--- CloudWeather/New Group/UI Pod/Animation View/Animation Files/LottieAnimationFile.swift
+++ CloudWeather/New Group/UI Pod/Animation View/Animation Files/LottieAnimationFile.swift
@@ -7,28 +7,45 @@
 //
 
 import UIKit
+import Lottie
 
 class LottieAnimationFile: AnimationFile {
 
     private var ready: Bool = false
-
     override var readyToPlay: Bool { return self.ready }
 
+    var lottieView = LOTAnimationView()
+    var owner: UIView?
+
     override func load(filePath: String, owner: UIView, loaded: LoadedClosure ) {
-        //setup and load
+        lottieView = LOTAnimationView(filePath: filePath)
+        lottieView.frame = owner.bounds
+        owner.addSubview(lottieView)
+
+        self.owner = owner
         ready = true
         loaded(true, nil)
     }
 
     override func play() {
-
+        guard owner != nil, ready else { return }
+        lottieView.play { [weak self] (finished) in
+            if finished, self?.loop ?? false {
+                self?.play()
+            }
+        }
     }
 
     override func stop() {
-
+        lottieView.stop()
     }
 
     override func pause(auto: Bool = false) {
+        lottieView.pause()
+    }
 
+    override func resize() {
+        guard let bounds = owner?.bounds else { return }
+        lottieView.frame = bounds
     }
 }
